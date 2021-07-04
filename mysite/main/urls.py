c8cd40fca539a7161f9b8e3271a4cf695454146f
yash <yashgyy@gmail.com>
2021-07-04 16:03:10 +0530

    Run mysite/manage.py runserver

---
+++ mysite/main/urls.py
@@ -0,0 +1,11 @@
+#pth to different sites maiin app
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("<int:id>",views.index,name="index"),
+    path("",views.home,name="home"),
+    path("home/",views.home,name="home"),
+    path("create/",views.create,name="create")
+]
